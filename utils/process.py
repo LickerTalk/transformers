8b27a724b18ebbabe3f0619b090ff935775b8ebb
ydshieh <ydshieh@users.noreply.github.com>
2024-10-14 14:30:18 +0200

    run

--- utils/process.py
+++ utils/process.py
@@ -6,6 +6,7 @@ with open("new_model_failures_with_bad_commit.json") as fp:
     data = json.load(fp)
 
 
+# TODO: extend
 team_members = ["ydshieh", "zucchini-nlp"]
 
 # Counting the number of failures grouped by authors
@@ -15,7 +16,7 @@ for model, model_result in data.items():
         for failed_test in failed_tests:
             author = failed_test["author"]
 
-            if not author in team_members:
+            if author not in team_members:
                 author = failed_test["merged_by"]
 
             if author not in new_data:
@@ -24,14 +25,14 @@ for model, model_result in data.items():
 for author in new_data:
     new_data[author] = dict(new_data[author])
 
-# # Group by author
-# new_data_full = {author: deepcopy(data) for author in new_data}
-# for author, _data in new_data_full.items():
-#     for model, model_result in _data.items():
-#         for device, failed_tests in model_result.items():
-#             failed_tests = [x for x in failed_tests if x["author"] == author or x["merged_by"] == author]
-#             model_result[device] = failed_tests
-# print(json.dumps(new_data_full, indent=4))
+# Group by author
+new_data_full = {author: deepcopy(data) for author in new_data}
+for author, _data in new_data_full.items():
+    for model, model_result in _data.items():
+        for device, failed_tests in model_result.items():
+            failed_tests = [x for x in failed_tests if x["author"] == author or x["merged_by"] == author]
+            model_result[device] = failed_tests
+print(json.dumps(new_data_full, indent=4))
 
 # Add `GH_` prefix as keyword mention
 output = {}
